0a0f49b02571f707b4ed35414dfeec735168a050
0x676e67 <gngppz@gmail.com>
2024-06-04 00:26:12 +0800

    feat(proxy): Add Http-To-Socks5 proxy forward

--- src/proxy/extension.rs
+++ src/proxy/extension.rs
@@ -1,4 +1,4 @@
-use super::murmur;
+use super::{murmur, socks5::proto::UsernamePassword};
 use http::HeaderMap;
 use std::net::IpAddr;
 
@@ -12,15 +12,18 @@ pub trait Whitelist {
 }
 
 /// Enum representing different types of extensions.
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub enum Extensions {
     /// No extension.
     None,
     /// Session extension with a tuple of two 64-bit integers.
     Session((u64, u64)),
+    /// Http to Socks5 extension. e.g. host:port:username:password
+    HttpToSocks5(((String, u16), Option<UsernamePassword>)),
 }
 
 impl Extensions {
+    const HTTP_TO_SOCKS5_HEADER: &'static str = "http-to-socks5";
     const SESSION_ID_HEADER: &'static str = "session-id";
 
     fn new(s: &str) -> Self {
@@ -49,14 +52,23 @@ impl From<(&str, &str)> for Extensions {
             // If it does, remove the prefix from `s`.
             if let Some(s) = s.strip_prefix(prefix) {
                 // Then, remove the "-session-" character that follows the prefix.
-                let s = s.trim_start_matches("-session-");
-                // Return it wrapped in the `Session` variant of `AuthExpand`.
-                return Self::new(s);
+                if s.starts_with("-session-") {
+                    let s = s.trim_start_matches("-session-");
+                    // Return it wrapped in the `Session` variant of `Extensions`.
+                    return Self::new(s);
+                }
+
+                // Then, remove the "-h2s-" character that follows the prefix.
+                if s.starts_with("-h2s-") {
+                    let s = s.trim_start_matches("-h2s-");
+                    // Split host:port:username:password
+                    return parse_socks5_extension(s);
+                }
             }
         }
         // If the string `s` does not start with the prefix, or if the remaining string
         // after removing the prefix and "-" is empty, return the `None` variant
-        // of `AuthExpand`.
+        // of `Extensions`.
         Extensions::None
     }
 }
@@ -67,14 +79,80 @@ impl From<&mut HeaderMap> for Extensions {
         if let Some(value) = headers.get(Self::SESSION_ID_HEADER) {
             // Convert the value to a string.
             if let Ok(s) = value.to_str() {
-                // Return it wrapped in the `Session` variant of `AuthExpand`.
+                // Return it wrapped in the `Session` variant of `Extensions`.
                 let extensions = Self::new(s);
                 headers.remove(Self::SESSION_ID_HEADER);
                 return extensions;
             }
         }
+        // Get the value of the `http-to-socks5` header from the headers.
+        if let Some(value) = headers.get(Self::HTTP_TO_SOCKS5_HEADER) {
+            // Convert the value to a string.
+            if let Ok(s) = value.to_str() {
+                // Split host:port:username:password
+                let extensions = parse_socks5_extension(s);
+                headers.remove(Self::HTTP_TO_SOCKS5_HEADER);
+                return extensions;
+            }
+        }
         // If the `session-id` header is not present, or if the value is not a valid
-        // string, return the `None` variant of `AuthExpand`.
+        // string, return the `None` variant of `Extensions`.
         Extensions::None
     }
+}
+
+/// Parses a SOCKS5 extension string.
+///
+/// This function takes a string `s` and attempts to parse it into a SOCKS5
+/// extension. The string should be in the format `host:port` or
+/// `host:port:username:password`.
+///
+/// If the string is in the format `host:port`, this function will return a
+/// `Extensions::HttpToSocks5` variant containing a tuple `(host, port)` and
+/// `None`.
+///
+/// If the string is in the format `host:port:username:password`, this function
+/// will return a `Extensions::HttpToSocks5` variant containing a tuple `(host,
+/// port)` and a `Some(UsernamePassword)`.
+///
+/// If the string does not match either of these formats, this function will
+/// return `Extensions::None`.
+///
+/// # Arguments
+///
+/// * `s` - The string to parse.
+///
+/// # Returns
+///
+/// This function returns an `Extensions` enum. If the string can be
+/// successfully parsed, it will return a `Extensions::HttpToSocks5` variant.
+/// Otherwise, it will return `Extensions::None`.
+fn parse_socks5_extension(s: &str) -> Extensions {
+    let parts: Vec<&str> = s.split(":").collect();
+    match parts.len() {
+        2 => {
+            if let Ok(port) = parts[1].parse::<u16>() {
+                let host = parts[0];
+                return Extensions::HttpToSocks5(((host.to_string(), port), None));
+            }
+        }
+        4 => {
+            if let Ok(port) = parts[1].parse::<u16>() {
+                let host = parts[0];
+                let username = parts[2];
+                let password = parts[3];
+                return Extensions::HttpToSocks5((
+                    (host.to_string(), port),
+                    Some(UsernamePassword::new(
+                        username.to_string(),
+                        password.to_string(),
+                    )),
+                ));
+            }
+        }
+        _ => {}
+    }
+
+    // do nothing
+    Extensions::None
 }
